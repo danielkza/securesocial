1c73a18aafbe25f2ff7464304b1b5729cd7ab99b
Daniel Miranda <danielkza2@gmail.com>
2015-01-16 23:14:48 -0200

    Bump version

--- project/Common.scala
+++ project/Common.scala
@@ -1,5 +1,5 @@
 object Common {
-  def version = "master-SNAPSHOT"  
+  def version = "3.0.0-M2-2"  
   def playVersion = System.getProperty("play.version", "2.3.7")
   def scalaVersion =  System.getProperty("scala.version", "2.10.4")
 }
